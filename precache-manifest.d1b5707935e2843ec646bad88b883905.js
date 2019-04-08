53d2928e97d5917e9174097aa164d7f6c6fdc125
Laura S <lausciampa@yahoo.com.ar>
2019-04-08 10:12:37 +0100

    Updates

---
+++ precache-manifest.d1b5707935e2843ec646bad88b883905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "164610539585de83db24",
+    "url": "/team-picker-app/static/css/main.19dbdad4.chunk.css"
+  },
+  {
+    "revision": "164610539585de83db24",
+    "url": "/team-picker-app/static/js/main.deaaad19.chunk.js"
+  },
+  {
+    "revision": "0540107b0d7e882f9f0d",
+    "url": "/team-picker-app/static/js/runtime~main.7238a16f.js"
+  },
+  {
+    "revision": "3b7995136a78c6b03f95",
+    "url": "/team-picker-app/static/js/2.993b454b.chunk.js"
+  },
+  {
+    "revision": "9fae50cc53cc0c02e93a7b66e561f1ab",
+    "url": "/team-picker-app/static/media/teams-BackgroundImg.9fae50cc.jpg"
+  },
+  {
+    "revision": "554bb951ce9eeca21756dbc534e7a7cb",
+    "url": "/team-picker-app/static/media/home-BackgroundImg.554bb951.jpg"
+  },
+  {
+    "revision": "a9b4e4a9887af1aefdfba6e7282e5bb4",
+    "url": "/team-picker-app/index.html"
+  }
+];
